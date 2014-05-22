f4cddc1a5e6a434d4c00a1b6a0bb24438447a253
MicMun <MicMunze@gmail.com>
2014-05-22 19:32:43 +0200

    Little fixes and changes to actual android sdk added.

--- workdaystarget/src/main/java/de/micmun/android/workdaystarget/DayCalculator.java
+++ workdaystarget/src/main/java/de/micmun/android/workdaystarget/DayCalculator.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 by Michael Munzert
+ * Copyright 2013-2014 by Michael Munzert
  *
  * This program is free software: you can redistribute it and/or modify it under 
  * the terms of the GNU >General Public License as published by the 
@@ -38,157 +38,158 @@ import java.util.Locale;
  * Calculates the difference to target in days.
  *
  * @author MicMun
- * @version 1.0, 07.04.2013
+ * @version 1.2, 22.05.2014
  */
 public class DayCalculator {
-    private static final int POS_HOLIDAY = 7; // position of holiday
-    private static final int POS_SUNDAY = 6; // position of sunday
-    private Calendar today = null;
-    private Calendar target = null;
+   private static final String TAG = "DayCalculator";
+   private static final int POS_HOLIDAY = 7; // position of holiday
+   private static final int POS_SUNDAY = 6; // position of sunday
+   private Calendar today = null;
+   private Calendar target = null;
 
-    /**
-     * Creates a new DayCalculator with target date.
-     */
-    public DayCalculator() {
-        today = Calendar.getInstance();
-        target = Calendar.getInstance();
-        setMidnight(today);
-    }
+   /**
+    * Creates a new DayCalculator with target date.
+    */
+   public DayCalculator() {
+      today = Calendar.getInstance();
+      target = Calendar.getInstance();
+      setMidnight(today);
+   }
 
-    /**
-     * Returns the number of days between today and target, skipped days in
-     * appliance of checkedDays.
-     *
-     * @param t           Date of the target, to which to calculate the working days.
-     * @param checkedDays Array with boolean flags, which says if a weekday should count
-     *                    or not.
-     * @return days to target.
-     * @throws JSONException if an error occurs while getting the holidays from web service.
-     */
-    public int getDaysLeft(Date t, boolean[] checkedDays) throws JSONException {
-        // sets target time at midnight
-        target.setTime(t);
-        setMidnight(target);
+   /**
+    * Returns the number of days between today and target, skipped days in
+    * appliance of checkedDays.
+    *
+    * @param t           Date of the target, to which to calculate the working days.
+    * @param checkedDays Array with boolean flags, which says if a weekday should count
+    *                    or not.
+    * @return days to target.
+    * @throws JSONException if an error occurs while getting the holidays from web service.
+    */
+   public int getDaysLeft(Date t, boolean[] checkedDays) throws JSONException {
+      // sets target time at midnight
+      target.setTime(t);
+      setMidnight(target);
 
-        int sign = 1;
+      int sign = 1;
 
-        if (target.getTimeInMillis() < today.getTimeInMillis()) {
-            Calendar tmp = Calendar.getInstance();
-            tmp.setTimeInMillis(target.getTimeInMillis());
-            target.setTimeInMillis(today.getTimeInMillis());
-            today.setTimeInMillis(tmp.getTimeInMillis());
-            sign = -1; // negative days in the past
-        }
+      if (target.getTimeInMillis() < today.getTimeInMillis()) {
+         Calendar tmp = Calendar.getInstance();
+         tmp.setTimeInMillis(target.getTimeInMillis());
+         target.setTimeInMillis(today.getTimeInMillis());
+         today.setTimeInMillis(tmp.getTimeInMillis());
+         sign = -1; // negative days in the past
+      }
 
-        // Holidays
-        ArrayList<Date> holidays = getHolidays();
+      // Holidays
+      ArrayList<Date> holidays = getHolidays();
 
-        // current time
-        Calendar curr = Calendar.getInstance();
-        curr.setTime(today.getTime());
+      // current time
+      Calendar curr = Calendar.getInstance();
+      curr.setTime(today.getTime());
 
-        int count = 0;
+      int count = 0;
 
-        while (curr.getTimeInMillis() != target.getTimeInMillis()) {
-            curr.add(Calendar.DAY_OF_MONTH, 1);
-            int dayOfWeek = curr.get(Calendar.DAY_OF_WEEK);
+      while (curr.getTimeInMillis() != target.getTimeInMillis()) {
+         curr.add(Calendar.DAY_OF_MONTH, 1);
+         int dayOfWeek = curr.get(Calendar.DAY_OF_WEEK);
 
-            if (!checkedDays[POS_HOLIDAY] && holidays.contains(curr.getTime())) {
-                continue;
-            } else if (!checkedDays[POS_SUNDAY] && dayOfWeek == Calendar.SUNDAY) {
-                continue;
-            } else if (dayOfWeek != Calendar.SUNDAY
-                    && !checkedDays[dayOfWeek - Calendar.MONDAY]) {
-                continue;
+         if (!checkedDays[POS_HOLIDAY] && holidays.contains(curr.getTime())) {
+            continue;
+         } else if (!checkedDays[POS_SUNDAY] && dayOfWeek == Calendar.SUNDAY) {
+            continue;
+         } else if (dayOfWeek != Calendar.SUNDAY
+               && !checkedDays[dayOfWeek - Calendar.MONDAY]) {
+            continue;
+         }
+         count++;
+      }
+
+      count *= sign; // if target is in the past, sign == -1
+
+      return count;
+   }
+
+   /**
+    * Returns the list with holydays between now and target.
+    *
+    * @return the list with holydays between now and target.
+    * @throws JSONException if parsing the holidays fails.
+    */
+   private ArrayList<Date> getHolidays() throws JSONException {
+      ArrayList<Date> holidays = new ArrayList<Date>();
+
+      String basisUrl = "http://kayaposoft.com/enrico/json/v1.0/"
+            + "?action=getPublicHolidaysForDateRange&fromDate=%s&toDate=%s"
+            + "&country=ger&region=Bavaria";
+      SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy",
+            Locale.getDefault());
+
+      String url = String.format(basisUrl, sdf.format(today.getTime()),
+            sdf.format(target.getTime()));
+      BufferedReader br = null;
+      String text = null;
+
+      try {
+         URL httpUrl = new URL(url);
+         HttpURLConnection con = (HttpURLConnection) httpUrl.openConnection();
+         con.setRequestMethod("GET");
+         con.connect();
+         InputStream is = con.getInputStream();
+         br = new BufferedReader(new InputStreamReader(is));
+         StringBuffer sb = new StringBuffer();
+         String line;
+
+         while ((line = br.readLine()) != null) {
+            sb.append(line);
+            sb.append('\n');
+         }
+         br.close();
+         text = sb.toString();
+      } catch (MalformedURLException e) {
+         System.err.println("FEHLER: " + e.getMessage());
+         holidays = null;
+      } catch (IOException e) {
+         System.err.println("FEHLER: " + e.getMessage());
+         holidays = null;
+      } finally {
+         if (br != null) {
+            try {
+               br.close();
+            } catch (IOException ignored) {
             }
-            count++;
-        }
+         }
+      }
 
-        count *= sign; // if target is in the past, sign == -1
+      if (text != null) {
+         JSONArray array = new JSONArray(text);
+         for (int i = 0; i < array.length(); ++i) {
+            JSONObject h = array.getJSONObject(i);
+            JSONObject o = h.getJSONObject("date");
+            int day = o.getInt("day");
+            int month = o.getInt("month");
+            int year = o.getInt("year");
+            Calendar cal = Calendar.getInstance();
+            cal.set(Calendar.DAY_OF_MONTH, day);
+            cal.set(Calendar.MONTH, month - 1);
+            cal.set(Calendar.YEAR, year);
+            setMidnight(cal);
+            holidays.add(cal.getTime());
+         }
+      }
 
-        return count;
-    }
+      return holidays;
+   }
 
-    /**
-     * Returns the list with holydays between now and target.
-     *
-     * @return the list with holydays between now and target.
-     * @throws JSONException if parsing the holidays fails.
-     */
-    private ArrayList<Date> getHolidays() throws JSONException {
-        ArrayList<Date> holidays = new ArrayList<Date>();
-
-        String basisUrl = "http://kayaposoft.com/enrico/json/v1.0/"
-                + "?action=getPublicHolidaysForDateRange&fromDate=%s&toDate=%s"
-                + "&country=ger&region=Bavaria";
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy",
-                Locale.getDefault());
-
-        String url = String.format(basisUrl, sdf.format(today.getTime()),
-                sdf.format(target.getTime()));
-        BufferedReader br = null;
-        String text = null;
-
-        try {
-            URL httpUrl = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) httpUrl.openConnection();
-            con.setRequestMethod("GET");
-            con.connect();
-            InputStream is = con.getInputStream();
-            br = new BufferedReader(new InputStreamReader(is));
-            StringBuffer sb = new StringBuffer();
-            String line;
-
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-                sb.append('\n');
-            }
-            br.close();
-            text = sb.toString();
-        } catch (MalformedURLException e) {
-            System.err.println("FEHLER: " + e.getMessage());
-            holidays = null;
-        } catch (IOException e) {
-            System.err.println("FEHLER: " + e.getMessage());
-            holidays = null;
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException ignored) {
-                }
-            }
-        }
-
-        if (text != null) {
-            JSONArray array = new JSONArray(text);
-            for (int i = 0; i < array.length(); ++i) {
-                JSONObject h = array.getJSONObject(i);
-                JSONObject o = h.getJSONObject("date");
-                int day = o.getInt("day");
-                int month = o.getInt("month");
-                int year = o.getInt("year");
-                Calendar cal = Calendar.getInstance();
-                cal.set(Calendar.DAY_OF_MONTH, day);
-                cal.set(Calendar.MONTH, month - 1);
-                cal.set(Calendar.YEAR, year);
-                setMidnight(cal);
-                holidays.add(cal.getTime());
-            }
-        }
-
-        return holidays;
-    }
-
-    /**
-     * Sets the time of given calendar to midnight.
-     *
-     * @param c calendar to set the time.
-     */
-    private void setMidnight(Calendar c) {
-        c.set(Calendar.HOUR_OF_DAY, 0);
-        c.set(Calendar.MINUTE, 0);
-        c.set(Calendar.SECOND, 0);
-        c.set(Calendar.MILLISECOND, 0);
-    }
+   /**
+    * Sets the time of given calendar to midnight.
+    *
+    * @param c calendar to set the time.
+    */
+   private void setMidnight(Calendar c) {
+      c.set(Calendar.HOUR_OF_DAY, 0);
+      c.set(Calendar.MINUTE, 0);
+      c.set(Calendar.SECOND, 0);
+      c.set(Calendar.MILLISECOND, 0);
+   }
 }
--- workdaystarget/src/main/java/de/micmun/android/workdaystarget/DaysLeftProvider.java
+++ workdaystarget/src/main/java/de/micmun/android/workdaystarget/DaysLeftProvider.java
@@ -1,16 +1,16 @@
 /**
- * Copyright 2013 by Michael Munzert
- * 
+ * Copyright 2013-2014 by Michael Munzert
+ *
  * This program is free software: you can redistribute it and/or modify it under 
  * the terms of the GNU >General Public License as published by the 
  * Free Software Foundation, either version 3 of the License, or >
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but 
  * WITHOUT ANY WARRANTY; >without even the implied warranty of MERCHANTABILITY 
  * or FITNESS FOR A PARTICULAR PURPOSE. >See the GNU General Public License 
  * for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with 
  * this program. If not, see >http://www.gnu.org/licenses/.
  */
@@ -24,23 +24,21 @@ import android.content.Intent;
 
 /**
  * Provider for data of widget.
- * 
+ *
  * @author MicMun
- * @version 1.0, 18.03.2013
- * 
+ * @version 1.2, 22.05.2014
  */
 public class DaysLeftProvider extends AppWidgetProvider {
-	public static final Intent UPDATE = new Intent(DaysLeftService.ACTION_UPDATE);
-	private Context mContext;
-
-	/**
-	 * @see android.appwidget.AppWidgetProvider#onUpdate(android.content.Context,
-	 *      android.appwidget.AppWidgetManager, int[])
-	 */
-	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-			int[] appWidgetIds) {
-		mContext = context;
-		mContext.startService(UPDATE);
-	}
+   /**
+    * @see android.appwidget.AppWidgetProvider#onUpdate(android.content.Context,
+    * android.appwidget.AppWidgetManager, int[])
+    */
+   @Override
+   public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                        int[] appWidgetIds) {
+      Intent update = new Intent(context.getApplicationContext(),
+            DaysLeftService.class);
+      update.setAction(DaysLeftService.ACTION_UPDATE);
+      context.startService(update);
+   }
 }
